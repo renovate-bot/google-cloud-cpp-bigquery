550f30b3d6859e04abfd4e51c7fb7a1efe95239e
Aryan Naraghi <aryan.naraghi@gmail.com>
2019-10-04 22:30:04 -0700

    Add the gRPC protos as a dependency

---
+++ bazel/googleapis.BUILD
@@ -0,0 +1,24 @@
+package(default_visibility = ["//visibility:public"])
+
+load("@com_github_grpc_grpc//bazel:cc_grpc_library.bzl", "cc_grpc_library")
+
+cc_proto_library(
+    name = "bigquerystorage_cc_proto",
+    deps = ["//google/cloud/bigquery/storage/v1beta1:bigquerystorage_proto"],
+)
+
+cc_grpc_library(
+  name = "grpc_bigquerystorage_proto",
+  srcs = ["//google/cloud/bigquery/storage/v1beta1:bigquerystorage_proto"],
+  deps = [":bigquerystorage_cc_proto"],
+  grpc_only = True,
+)
+
+cc_library(
+  name = "bigquery_protos",
+  deps = [
+    ":grpc_bigquerystorage_proto",
+    "@com_github_grpc_grpc//:grpc++",
+    ],
+  includes = ["."],
+)
--- bigquery/internal/connection_impl.cc
+++ bigquery/internal/connection_impl.cc
@@ -1,3 +1,4 @@
+#include <google/cloud/bigquery/storage/v1beta1/storage.pb.h>
 #include <memory>
 
 #include "bigquery/internal/connection_impl.h"
@@ -7,11 +8,16 @@ namespace bigquery {
 inline namespace BIGQUERY_CLIENT_NS {
 namespace internal {
 
+namespace bigquerystorage_proto = ::google::cloud::bigquery::storage::v1beta1;
+
 using ::google::cloud::Status;
 using ::google::cloud::StatusCode;
 using ::google::cloud::StatusOr;
 
 StatusOr<std::string> ConnectionImpl::CreateSession(std::string table) {
+  bigquerystorage_proto::CreateReadSessionRequest request;
+  request.mutable_table_reference()->set_table_id(table);
+
   return Status(StatusCode::kUnimplemented,
                 "This function has not been implemented yet.");
 }
